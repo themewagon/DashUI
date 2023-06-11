7fed6496744e5218e14f510c5766164becd53eda
MostofaNobiTanveer <mntanveer9196@gmail.com>
2023-06-11 13:18:20 +0600

    live test wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["themewagon.github.io/DashUI"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
