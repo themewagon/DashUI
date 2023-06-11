e0c08dd636a4c72a147065016cda4483f347fb93
MostofaNobiTanveer <mntanveer9196@gmail.com>
2023-06-11 13:14:17 +0600

    live test wip

--- sub-components/profile/ProfileHeader.js
+++ sub-components/profile/ProfileHeader.js
@@ -14,7 +14,7 @@ const ProfileHeader = () => {
             <div className="d-flex align-items-center">
               {/* avatar */}
               <div className="avatar-xxl avatar-indicators avatar-online me-2 position-relative d-flex justify-content-end align-items-end mt-n10">
-                <Image src="/images/avatar/avatar-1.jpg" className="avatar-xxl rounded-circle border border-4 border-white-color-40" alt="" />
+                <Image src="images/avatar/avatar-1.jpg" className="avatar-xxl rounded-circle border border-4 border-white-color-40" alt="" />
                 <Link href="#!" className="position-absolute top-0 right-0 me-2" data-bs-toggle="tooltip" data-placement="top" title="" data-original-title="Verified">
                   <Image src="/images/svg/checked-mark.svg" alt="" height="30" width="30" />
                 </Link>
